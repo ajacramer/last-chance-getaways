b36fd6c165b71de8456a7df7f87235f8046acc85
Aja Cramer <azc6393@psu.edu>
2025-04-15 18:24:53 -0400

    CSS styling_4.14.25

--- .eleventy.js
+++ .eleventy.js
@@ -1,20 +1,10 @@
 module.exports = function (eleventyConfig) {
-    return {
-      dir: {
-        input: "src",
-        output: "_site"
-      }
-    };
+  eleventyConfig.addPassthroughCopy("src/assets");
+
+  return {
+    dir: {
+      input: "src",
+      output: "_site"
+    }
   };
-  
-  module.exports = function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("src/assets");
-  
-    return {
-      dir: {
-        input: "src",
-        output: "_site"
-      }
-    };
-  };
-  
+};
